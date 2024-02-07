6296105bbeed682c21146c126512e1e9227bb770
sourav-k7 <neymarsk01@gmail.com>
2024-02-08 02:22:41 +0530

    google ownership

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx:any) {
+  static async getInitialProps(ctx: any) {
     const originalRenderPage = ctx.renderPage
 
     // Run the React rendering logic synchronously
     ctx.renderPage = () =>
       originalRenderPage({
         // Useful for wrapping the whole react tree
-        enhanceApp: (App:any) => App,
+        enhanceApp: (App: any) => App,
         // Useful for wrapping in a per-page basis
-        enhanceComponent: (Component:any) => Component,
+        enhanceComponent: (Component: any) => Component,
       })
 
     // Run the parent `getInitialProps`, it now includes the custom `renderPage`
@@ -23,11 +23,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head >
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-       
-          </Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+          <meta name="google-site-verification" content="QJXpLHA8nW53EkK01ceXcXiRLhFzZ7BpQ262oVT3-lg" />
+        </Head>
         <body>
           <Main />
           <NextScript />

5551222032c785154e24a18f5374db30f6bae420
sourav-k7 <neymarsk01@gmail.com>
2024-03-31 22:54:23 +0530

    month in previously completed list

--- components/previously_completed_task.tsx
+++ components/previously_completed_task.tsx
@@ -15,32 +15,47 @@ export default function PreviouslyCompletedTask() {
 
 
 	useEffect(() => {
-		if(selectedProject!=null){
+		if (selectedProject != null) {
 			dispatch<any>(getAllPreviouslyCompletedTask({ skip: 0, project: selectedProject?._id } as PreviousTaskServiceType));
 		}
 	}, [selectedProject]);
 
 	function handleLoadMore() {
 		let skip = previouslyCompletedTask.filter(tk => tk.project == selectedProject?._id).length;
-		dispatch<any>(getAllPreviouslyCompletedTask({ skip:skip , project: selectedProject?._id } as PreviousTaskServiceType));
+		dispatch<any>(getAllPreviouslyCompletedTask({ skip: skip, project: selectedProject?._id } as PreviousTaskServiceType));
 	}
 
+	function getPerviouslyCompleteList() {
+
+	}
 	return (
 		<ToggleTaskList title={'Previously completed task'}>
 
 			<div>
 				{previouslyCompletedTask
 					.filter(tk => tk.project == selectedProject?._id)
-					.map((tk) =>
-						<TaskTitleTile
-							title={tk.task}
-							id={tk._id}
-							key={tk._id}
-							point={tk.point}
-							handleClick={() =>{ dispatch(selectTask({ id: tk._id, type: ListType.previous } as SelectTaskPayloadType))}}
-							handleDrag={() => { setDragId(tk._id) }}
-							handleDrop={() => { handleDrop(tk._id) }}
-						/>
+					.map((tk, index, filterprvTaskList) => {
+						const currentDate = new Date(tk.completionTime);
+						const currentMonth = currentDate.toLocaleString('default', { month: 'short' });
+						const currentYear = currentDate.getFullYear();
+						const shouldDisplayMonth = index === 0 || currentMonth !== new Date(filterprvTaskList[index - 1].completionTime).toLocaleString('default', { month: 'short' });
+						return (
+							<div
+								key={tk._id}
+							>
+								{shouldDisplayMonth && (
+									<div className='text-sm text-gray-600'>{currentMonth} {currentYear}</div>
+								)}
+								<TaskTitleTile
+									title={tk.task}
+									id={tk._id}
+									point={tk.point}
+									handleClick={() => { dispatch(selectTask({ id: tk._id, type: ListType.previous } as SelectTaskPayloadType)) }}
+									handleDrag={() => { setDragId(tk._id) }}
+									handleDrop={() => { handleDrop(tk._id) }}
+								/>
+							</div>)
+					}
 					)}
 
 				<button className='btn-primary w-full' onClick={handleLoadMore}>
